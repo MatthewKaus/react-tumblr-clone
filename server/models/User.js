b2a33646b80ead8bf27437e50b991de300426d90
Matthew Kaus <matthewkausfox@gmail.com>
2022-09-30 06:00:15 -0600

    added post.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,46 +1,54 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt')
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // const Schema = require('./schema')
 
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            match: [/.+@.+\..+/, 'Must use a valid email address'],
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        // savedSchema: [schema],
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    {
-        toJSON: {
-            virtuals: true,
-        },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must use a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+        type: String,
+        required: true
     }
+    // savedSchema: [schema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
 );
 
 // hash user password
-userSchema.pre('save', async function (next) {
-    if (this.isNew || this.isModified('password')) {
-        const saltRounds = 10;
-        this.password = await bcrypt.hash(this.password, saltRounds);
-    }
-    next();
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
 });
 
 // custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
-    return bcrypt.compare(password, this.password);
+  return bcrypt.compare(password, this.password);
 };
 
 // when we query a user, we'll also get another field called `stockCount` with the number of saved stocks we have
@@ -48,6 +56,6 @@ userSchema.methods.isCorrectPassword = async function (password) {
 //     return this.savedStocks.length;
 // });
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
